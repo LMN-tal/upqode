2379a14a00f43bd857cabb6e550ca4d745100653
Ivan Klevets <ivan.klevets@gmail.com>
2020-12-18 19:39:57 +0200

    first commit

---
+++ upqode_frontend/gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp'),
+    gp = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });  
+})
+
+gulp.task('pug', function () {
+  return gulp.src('src/pug/pages/*.pug')
+    .pipe(gp.pug({pretty: true}))
+    .pipe(gulp.dest('build'))
+    .on('end',browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/static/sass/*.scss')
+    .pipe(gp.sourcemaps.init())
+    .pipe(gp.sass().on('error', gp.sass.logError))
+    .pipe(gp.autoprefixer({ cascade: false }))
+    .pipe(gp.csso())
+    .pipe(gp.sourcemaps.write())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({
+      stream:true
+    }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/pug/**/*.pug',gulp.series('pug'));
+    gulp.watch('src/static/sass/**/*.scss',gulp.series('sass'));
+})
+
+gulp.task('default', gulp.series(
+  gulp.parallel('pug','sass'),
+  gulp.parallel('watch','serve')
+  ));
